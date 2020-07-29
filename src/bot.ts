65bcd24013c11a1151e09d00db1d51167f653179
Guilherme Oliveira <gui.r.oliveira@outlook.com>
2020-07-28 23:34:14 -0300

    :yay: Basic configs added

---
+++ src/bot.ts
@@ -0,0 +1,9 @@
+const { token, owners } = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.login(token);
